51c56cf1683297cf10dfe0b38b08406a9720ffb6
rohansinha01 <rohansinha01@gmail.com>
2024-03-26 22:03:51 -0400

    wip: update and delete turtles

--- src/actions.jsx
+++ src/actions.jsx
@@ -25,6 +25,37 @@ export const createAction = async ({request}) => {
     // redirect back to the index
     return redirect("/")
 }
-// Update Action
+// Update Action updating a pet from a form submission to `update/:id`
+export const updateAction = async ({request, params}) => {
+    // get the form data
+    const formData = await request.formData()
+    // get the pet id
+    const id = params.id
+    // construct the request body
+    const updatedPet = {
+        name: formData.get("name"),
+        age: formData.get("age"),
+    }
+    // send the request body to the backend
+    await fetch(`${URL}${id}/`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedPet)
+    })
+    // redirect to the Show page
+    return redirect(`/pets/${id}`)
+}
 
-// Delete Action
+// Delete Action rehome a pet from a form submission to `delete/:id`
+export const deleteAction = async ({params}) => {
+    // get the pet id
+    const id = params.id
+    // send the request to the backend
+    await fetch(`${URL}${id}/`, {
+        method: "DELETE"
+    })
+    // redirect to the index page
+    return redirect("/")
+}
